8572a8bb65199f87f8ead3944b53f7e01ac25ea3
Andrew Raines <andrew@raines.me>
2013-10-13 22:36:41 +0200

    Test different queue sizes.

--- test/workroom/test/core.clj
+++ test/workroom/test/core.clj
@@ -12,42 +12,48 @@
      ~@body
      (log/log ~op (- (System/currentTimeMillis) start#) "ms")))
 
+(defn test [msgs]
+  (testing msgs
+    (let [q (work/declare-exchange
+             (work/->Queue "http://localhost:9200" (rand-queue) "test.foo")
+             :store :ram)
+          pool (Executors/newFixedThreadPool
+                (int (/ (.availableProcessors (Runtime/getRuntime)) 2)))
+          consumed (java.util.concurrent.CountDownLatch. msgs)
+          n (atom 0)
+          xs (atom (sorted-set))
+          ms (atom [])
+          go (fn [latch]
+               (fn [msg]
+                 (when msg
+                   #_(log/log 'consume (-> msg :_source))
+                   (swap! ms conj msg)
+                   (swap! n + (-> msg :_source :n))
+                   (swap! xs conj (-> msg :_source :x))
+                   (.countDown latch))))]
+      (time* 'publish
+             (work/publish-seq q (for [x (range msgs)]
+                                   {:n 1 :x x})))
+      (time* 'consume
+             (dotimes [n msgs]
+               '(log/log 'remain n (work/queue-size q))
+               (.execute pool
+                         (fn []
+                           (work/consume q 10 500 (go consumed))))
+               (Thread/sleep (rand-int 5)))
+             (.await consumed))
+      (log/log pool)
+      (is (= msgs @n))
+      (is (= msgs (count @xs)))
+      (is (= (apply sorted-set (range msgs)) @xs))
+      (if (= msgs (count @xs))
+        (doseq [m @ms]
+          (work/ack m)))
+      (is (zero? (work/queue-size q)))
+      (work/delete-queue q))))
+
 (deftest integrate!
-  (let [q (work/declare-exchange
-           (work/->Queue "http://localhost:9200" (rand-queue) "test.foo")
-           :store :ram)
-        msgs 100
-        pool (Executors/newFixedThreadPool
-              (int (/ (.availableProcessors (Runtime/getRuntime)) 2)))
-        consumed (java.util.concurrent.CountDownLatch. msgs)
-        n (atom 0)
-        xs (atom (sorted-set))
-        ms (atom [])
-        go (fn [latch]
-             (fn [msg]
-               (when msg
-                 #_(log/log 'consume (-> msg :_source))
-                 (swap! ms conj msg)
-                 (swap! n + (-> msg :_source :n))
-                 (swap! xs conj (-> msg :_source :x))
-                 (.countDown latch))))]
-    (time* 'publish
-      (work/publish-seq q (for [x (range msgs)]
-                            {:n 1 :x x})))
-    (time* 'consume
-      (dotimes [_ msgs]
-        #_(log/log 'remain (work/queue-size q))
-        (.execute pool
-                  (fn []
-                    (work/consume q 10 500 (go consumed))))
-        #_(Thread/sleep (rand-int 5)))
-      (.await consumed))
-    (log/log pool)
-    (is (= msgs @n))
-    (is (= msgs (count @xs)))
-    (is (= (apply sorted-set (range msgs)) @xs))
-    (if (= msgs (count @xs))
-      (doseq [m @ms]
-        (work/ack m)))
-    (is (zero? (work/queue-size q)))
-    (work/delete-queue q)))
+  (test 1)
+  (test 10)
+  (test 100)
+  (test 500))

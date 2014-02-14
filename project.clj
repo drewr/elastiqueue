eb05053717d52481100faff641112c76fd9cd97d
Andrew Raines <andrew@raines.me>
2014-02-14 15:08:50 -0600

    0.99.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.elasticsearch/elastiqueue "0.99.0-SNAPSHOT"
+(defproject org.elasticsearch/elastiqueue "0.99.1"
   :description "You know... for messaging"
   :url "https://github.com/drewr/elastiqueue"
   :license {:name "The Apache 2 License"
